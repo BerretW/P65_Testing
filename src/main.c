63709883e83fdef04fe8c09f6f76618b1d860790
adolf01 <adolf01@seznam.cz>
2021-01-13 23:12:50 +0100

    main.c cleanup

--- src/main.c
+++ src/main.c
@@ -3,22 +3,8 @@
 #include <string.h>
 
 
-//#include "acia.h"
-//#include "lcd.h"
-//#include "ym2612.h"
-#include "utils.h"
-#include "kernel_c_lib.h"
-//#include "song.h"
-//#include "vdp_low.h"
-//#include "test.h"
-//#include "bank.h"
-//#include "wait.h"
-//
 //#include "utils.h"
-//#include "wood_char.h"
-//#include "sprite.h"
-//#include "copyshort.h"
-
+#include "kernel_c_lib.h"
 
 
 void main(void) {
@@ -28,5 +14,4 @@ void main(void) {
   while(1){
     CHROUT(KBINPUT());
   }
-
 }
